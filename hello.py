e57a63a29fc9bf15ddd62cde6163ae4414838160
Bill Zhuang <zhuangmin@gmail.com>
2014-07-01 23:51:26 +0800

    uu

--- hello.py
+++ hello.py
@@ -384,7 +384,7 @@ def list_message():
     for item in msgList:
         item.sender = unicode(item.sender, 'utf-8')
 
-    return render_template('message.html', entries=msgList)
+    return render_template('_message.html', entries=msgList)
 
 @app.route('/feed')
 def feed():
@@ -433,7 +433,7 @@ def feed():
         msg.name = unicode(msg.name, 'utf-8')
         msg.content = unicode(msg.content, 'utf-8')
 
-    return render_template('feed.html'
+    return render_template('_feed.html'
         , team=team,canfinish=canfinish
         , showsummary=showsummary
         , teamsummary=teamsummary
